77a0a606f0d8c845de7a90a8049e80ae1c45e357
Jason <jasonlittle.user@gmail.com>
2026-07-03 17:03:26 +1200

    All status bars for player added

--- src/game/GameHUD.cpp
+++ src/game/GameHUD.cpp
@@ -9,23 +9,53 @@ static float EaseOut(float t)
 }
 
 
-void GameHUD::Update(float dt, float newHealthPerc)
+void GameHUD::Update(float dt, Character& player)
 {
-    m_displayHealth = std::lerp(m_displayHealth, newHealthPerc, EaseOut(dt) * 2.f);
+    float newHealthPerc = player.Health() / player.Stats().MaxHealth;
+    m_displayHealth = std::lerp(m_displayHealth, newHealthPerc, EaseOut(dt) * 2);
+    m_displayHealthMax = player.Stats().MaxHealth;
+
+    float newStaminaPerc = player.Stamina() / player.Stats().MaxStamina;
+    m_displayStamina = std::lerp(m_displayStamina, newStaminaPerc, EaseOut(dt) * 2);
+    m_displayStaminaMax = player.Stats().MaxStamina;
+
+    float newManaPerc = player.Mana() / player.Stats().MaxMana;
+    m_displayMana = std::lerp(m_displayMana, newManaPerc, EaseOut(dt) * 2);
+    m_displayManaMax = player.Stats().MaxMana;
+
 }
 
 
 void GameHUD::DrawHUD(QuadRenderer& quadRenderer, OrthographicCamera camera)
 {
-    glm::vec2 barSize = glm::vec2(200, 10);
+    glm::vec2 healthBarSize = glm::vec2(m_displayHealthMax * 2, 10);
+    glm::vec2 healthBarPosition = camera.Pos + glm::vec2(-camera.Size.x * 0.5 + 5, camera.Size.y * 0.5 - healthBarSize.y - 5);
+    float healthBarFill = healthBarSize.x * m_displayHealth;
+    if (healthBarFill < 0)
+        healthBarFill = 0;
+    glm::vec2 healthFillSize = glm::vec2(healthBarFill, healthBarSize.y);
 
-    float healthBarSize = barSize.x * m_displayHealth;
-    if (healthBarSize < 0)
-        healthBarSize = 0;
+    quadRenderer.Render(camera, healthBarPosition, healthBarSize, glm::vec4(0.12f, 0.12f, 0.13f, 1.0f));
+    quadRenderer.Render(camera, healthBarPosition, healthFillSize, glm::vec4(0.18f, 0.02f, 0.02f, 1.0f), glm::vec4(0.55f, 0.05f, 0.05f, 1.0f), glm::vec4(0.75f, 0.12f, 0.08f, 1.0f));
 
-    glm::vec2 healthSize = glm::vec2(healthBarSize, 10);
+    
+    glm::vec2 staminaBarSize = glm::vec2(m_displayStaminaMax * 1.5, 10);
+    glm::vec2 staminaBarPosition = camera.Pos + glm::vec2(-camera.Size.x * 0.5 + 5, camera.Size.y * 0.5 - staminaBarSize.y - healthBarSize.y - 7);
+    float staminaBarFill = staminaBarSize.x * m_displayStamina;
+    if (staminaBarFill < 0)
+        staminaBarFill = 0;
+    glm::vec2 staminaFillSize = glm::vec2(staminaBarFill, staminaBarSize.y);
 
+    quadRenderer.Render(camera, staminaBarPosition, staminaBarSize, glm::vec4(0.12f, 0.12f, 0.13f, 1.0f));
+    quadRenderer.Render(camera, staminaBarPosition, staminaFillSize, glm::vec4(0.18f, 0.26f, 0.08f, 1.0f), glm::vec4(0.45f, 0.62f, 0.18f, 1.0f), glm::vec4(0.65f, 0.80f, 0.28f, 1.0f));
 
-    quadRenderer.Render(camera, camera.Pos + glm::vec2(-camera.Size.x * 0.5 + 5, camera.Size.y * 0.5 - barSize.y - 5), barSize, glm::vec4(0.15f, 0.15f, 0.15f, 1.0f));
-    quadRenderer.Render(camera, camera.Pos + glm::vec2(-camera.Size.x * 0.5 + 5, camera.Size.y * 0.5 - barSize.y - 5), healthSize, glm::vec4(0.18f, 0.02f, 0.02f, 1.0f), glm::vec4(0.55f, 0.05f, 0.05f, 1.0f), glm::vec4(0.75f, 0.12f, 0.08f, 1.0f));
+    glm::vec2 manaBarSize = glm::vec2(m_displayManaMax * 1, 10);
+    glm::vec2 manaBarPosition = camera.Pos + glm::vec2(-camera.Size.x * 0.5 + 5, camera.Size.y * 0.5 - manaBarSize.y - staminaBarSize.y - healthBarSize.y - 10);
+    float manaBarFill = manaBarSize.x * m_displayMana;
+    if (manaBarFill < 0)
+        manaBarFill = 0;
+    glm::vec2 manaFillSize = glm::vec2(manaBarFill, manaBarSize.y);
+
+    quadRenderer.Render(camera, manaBarPosition, manaBarSize, glm::vec4(0.12f, 0.12f, 0.13f, 1.0f));
+    quadRenderer.Render(camera, manaBarPosition, manaFillSize, glm::vec4(0.05f, 0.18f, 0.38f, 1.0f), glm::vec4(0.18f, 0.48f, 0.78f, 1.0f), glm::vec4(0.35f, 0.70f, 0.95f, 1.0f));
 }
--- src/game/include/ui/GameHUD.hpp
+++ src/game/include/ui/GameHUD.hpp
@@ -2,16 +2,24 @@
 
 #include "SpriteRenderer.hpp"
 #include "QuadRenderer.hpp"
+#include "Character.hpp"
 
 
 class GameHUD
 {
 public:
 
-    void Update(float dt, float newHealthPerc);
+    void Update(float dt, Character& player);
     void DrawHUD(QuadRenderer& quadRenderer, OrthographicCamera camera);
 
 private:
     float m_displayHealth = 0;
+    float m_displayHealthMax = 0;
+
+    float m_displayStamina = 0;
+    float m_displayStaminaMax = 0;
+
+    float m_displayMana = 0;
+    float m_displayManaMax = 0;
 
 };
